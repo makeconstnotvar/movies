1c3de7dd25d87994cfbf2004cfe845f59b21198d
Alexander <akotlyachkov@gmail.com>
2020-03-14 15:18:27 +0300

    design

--- app/controls/Header.jsx
+++ app/controls/Header.jsx
@@ -3,15 +3,15 @@ import {Link} from "@reach/router";
 import {AboutPage} from "../pages/AboutPage";
 
 const Header = props => (
-  <Fragment>
-    <div>Header</div>
-    <div>
-      <Link to="/">Main</Link>
-      <Link to="/movies">Movies</Link>
-      <Link to="/about">About</Link>
-      <Link to="/responsive">Responsive</Link>
+  <div className="header">
+    <div className="container header-logo fs-32">Header</div>
+    <div className="container">
+      <Link className="header-link" to="/">Main</Link>
+      <Link className="header-link" to="/movies">Movies</Link>
+      <Link className="header-link" to="/about">About</Link>
+      <Link className="header-link" to="/responsive">Responsive</Link>
     </div>
-  </Fragment>
+  </div>
 );
 
 export {Header}
