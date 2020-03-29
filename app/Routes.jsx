361c3ba31a72b066e552170300c4b75bc8d80917
Alexander <akotlyachkov@gmail.com>
2020-03-29 22:36:19 +0300

    react-router

--- app/Routes.jsx
+++ app/Routes.jsx
@@ -1,13 +1,8 @@
 import {Component} from "react";
 import React from 'react';
-import {BrowserRouter, Route, Switch} from "react-router-dom";
+import {BrowserRouter, Switch} from "react-router-dom";
 import {routes, RoutesMap} from './routes';
-import {AboutPage} from "pages/AboutPage";
-import {MoviesTopPage} from "pages/MoviesTopPage";
-import {MovieDetailsPage} from "pages/MovieDetailsPage";
-import {MainPage} from "pages/MainPage";
 import {MasterLayout} from "layouts/MasterLayout";
-import {ResponsivePage} from "pages/ResponsivePage";
 
 class Routes extends Component {
   componentDidMount() {
